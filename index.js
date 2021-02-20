e325370599121257d9dd9d10f8f4fb202347a719
Michael Olmos Trujillo <molmost@unal.edu.co>
2021-02-19 21:55:18 -0500

    creating files, canvas and canvas styles

---
+++ index.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas1');
+//API 2d of canvas
+const ctx = canvas.getContext('2d');
+//canvas cover the entire browser window
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
